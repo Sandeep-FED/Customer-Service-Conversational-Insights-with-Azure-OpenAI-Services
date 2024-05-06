8466f40bec915716c06bdd8a514d09d7529ce204
Sandeep P S <sandymankara11@gmail.com>
2024-05-06 17:12:25 +0530

    feat: add user authentication module

---
+++ GBB.ConversationalKM.WebUI/CognitiveSearch.UI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CognitiveSearch.UI.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CognitiveSearch.UI.Areas.Identity.IdentityHostingStartup))]
+namespace CognitiveSearch.UI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CognitiveSearchUIContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("CognitiveSearchUIContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<CognitiveSearchUIContext>();
+            });
+        }
+    }
+}
---
+++ GBB.ConversationalKM.WebUI/CognitiveSearch.UI/Views/Shared/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CognitiveSearch.UI.Views.Shared
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
